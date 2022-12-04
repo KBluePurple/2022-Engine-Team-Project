dd8625f19ea732febbe4a2fb01ec5556606858d1
zanke27 <zanke27@gmail.com>
2022-12-04 23:42:24 +0900

    Feel 에셋 추가
    가능한 필요 없는 기능 쳐냈음

---
+++ Assets/Scripts/FeelTest.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using MoreMountains.Feedbacks;
+
+public class FeelTest : MonoBehaviour
+{
+    /*
+    public MMFeedbacks jumpFeedBack;
+    public MMFeedbacks landFeedBack;
+
+    private Rigidbody rb = null;
+
+    private bool isJumping = false;
+    private float lastVelocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = Vector3.down * 30;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isJumping == false)
+        {
+            rb.AddForce(Vector3.up * 7.5f, ForceMode.Impulse);
+            jumpFeedBack?.PlayFeedbacks();
+            isJumping = true;
+        }
+
+        if (isJumping && (lastVelocity < 0) && (Mathf.Abs(rb.velocity.y) < 0.1))
+        {
+            isJumping = false;
+            landFeedBack?.PlayFeedbacks();
+        }
+
+        lastVelocity = rb.velocity.y;
+    }
+    */
+}
