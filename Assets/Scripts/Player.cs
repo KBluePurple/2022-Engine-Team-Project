a529ddc04554cf9569eddbc5bdd44f50dbdf2cd4
zanke27 <zanke27@gmail.com>
2022-12-21 12:54:15 +0900

    은신시 구분가능하게 머태리얼 바꿈

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour, IHitAble
     [SerializeField] private GameObject dashParticle; 
     [SerializeField] private float bombRadius = 27.5f;
     [SerializeField] private LayerMask bulletLayer;
+    private Renderer playerRenderer;
 
 
     [SerializeField] private SkillBase[] skills = new SkillBase[4];
@@ -48,6 +49,7 @@ public class Player : MonoBehaviour, IHitAble
 
     private void Awake()
     {
+        playerRenderer = GetComponent<Renderer>();
         _mainCamera = Camera.main;
         _hpBar = transform.Find("Health Bar Canvas").GetComponent<HpBar>();
         _nowHp = _maxHp;
@@ -175,7 +177,9 @@ public class Player : MonoBehaviour, IHitAble
     private IEnumerator StealthCoroutine()
     {
         _isInvincibility = true;
+        playerRenderer.material.color = Color.gray;
         yield return new WaitForSeconds(3f);
+        playerRenderer.material.color = Color.white;
         _isInvincibility = false;
     }
 
